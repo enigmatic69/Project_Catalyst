bf7102318d3edbed9f7391cc4d746b0f1246795f
enigmatic69 <156055411+enigmatic69@users.noreply.github.com>
2024-06-08 14:00:04 +0800

    Update HTML templates. (Sat 08 Jun 2024 02:00:01 PM +08)

--- Project_Catalyst/ThisAddIn.cs
+++ Project_Catalyst/ThisAddIn.cs
@@ -36,24 +36,4 @@ namespace Project_Catalyst
         }
     }
 }
-// Sat 01 Jun 2024 02:00:01 PM +08
-// Sat 01 Jun 2024 04:00:01 PM +08
-// Sun 02 Jun 2024 10:00:01 AM +08
-// Sun 02 Jun 2024 02:00:01 PM +08
-// Sun 02 Jun 2024 04:00:01 PM +08
-// Mon 03 Jun 2024 10:00:01 AM +08
-// Mon 03 Jun 2024 02:00:01 PM +08
-// Mon 03 Jun 2024 04:00:01 PM +08
-// Tue 04 Jun 2024 10:00:01 AM +08
-// Tue 04 Jun 2024 02:00:01 PM +08
-// Tue 04 Jun 2024 04:00:01 PM +08
-// Wed 05 Jun 2024 10:00:01 AM +08
-// Wed 05 Jun 2024 02:00:01 PM +08
-// Wed 05 Jun 2024 04:00:01 PM +08
-// Thu 06 Jun 2024 10:00:02 AM +08
-// Thu 06 Jun 2024 02:00:01 PM +08
-// Thu 06 Jun 2024 04:00:01 PM +08
-// Fri 07 Jun 2024 10:00:01 AM +08
-// Fri 07 Jun 2024 02:00:01 PM +08
-// Fri 07 Jun 2024 04:00:01 PM +08
-// Sat 08 Jun 2024 10:00:01 AM +08
+// Sat 08 Jun 2024 02:00:01 PM +08
